92efda9880e0b10a62f68e26a991b2761f24eb1d
vasfvitor <gitkey@virtuaires.com.br>
2024-01-20 18:02:18 -0300

    refactor error handling

--- main.py
+++ main.py
@@ -1,69 +1,99 @@
-"""Script bla bla bla"""
+"""Script pra conectar planilha de controle a um banco Postgres"""
 import os
 import pandas as pd
 import psycopg2
-from sqlalchemy import create_engine
+import logging
+from sqlalchemy import create_engine, inspect
 
-# tem que fazer uma modificação pra ler a partir da 3 linha (ou outra dependendo da planilha)
-START = 2
-
-PG_ENGINE = os.getenv('PG_ENGINE')
-PG_CONN = os.getenv('PG_CONN')
+PG_ENGINE = os.getenv("PG_ENGINE")
+PG_CONN = os.getenv("PG_CONN")
+TO_SKIP = 3
 
 
 def main():
-    """Função que lê a planinha, cria a tabela e manda os dados pro banco Postgres."""
-    # Ler a planilha com pandas, no caso a primeira folha e pula START linhas
-    df = pd.read_excel("Controle2.xlsx", 0, skiprows=START)
+    """Função que lê a planilha, cria a tabela e manda os dados pro banco Postgres."""
+    logging.basicConfig(level=logging.INFO)
 
-    # Nao sei se ta correto, tive que usar o engine separado pq o Pandas tava reclamando que tinha só suporta
+
+    # Le a planilha, primeira folha (0), pula TO_SKIP linhas (conta o cabeçalho pra não incluir ele)
+    try:
+        logging.info("Lendo planilha com pandas")
+        df = pd.read_excel(
+            "Controle.xlsm",
+            0,
+            skiprows=TO_SKIP,
+            header=None,
+            names=["codigo", "data", "autor", "titulo", "link"],
+        )
+        logging.info("Ok")
+    except Exception as e:
+        logging.error(f"Erro ao ler a planilha: {e}")
+        return
+
+    # Nao sei se ta correto, tive que usar o engine separado pq o
+    # Pandas tava reclamando que tinha só suporta
     # sqlalchemy e outros engines. Taentar depois só com o psycopg2
     conn = psycopg2.connect(PG_CONN)
     engine = create_engine(PG_ENGINE)
 
     # Create a database cursor
-    cur = conn.cursor()
+    cursor = conn.cursor()
 
-    # Criar tabela no db
-    cur.execute(
-        """CREATE TABLE IF NOT EXISTS controle_material (
-            codigo INT PRIMARY KEY,
-            data DATE,
-            autor VARCHAR(255),
-            titulo VARCHAR(255),
-            link VARCHAR(255))
-            """
-    )
+    try:
+        print("Criando banco")
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS controle_material (
+                id SERIAL PRIMARY KEY,
+                codigo INT,
+                data DATE,
+                autor VARCHAR(255),
+                titulo VARCHAR(255),
+                link VARCHAR(255)
+            )"""
+        )
+        conn.commit()
 
+    except Exception as e:
+        print(f"Erro ao criar banco: {e}")
+    finally:
+        print("Ok")
 
+    try:
+        inspector = inspect(engine)
+        if not inspector.has_table("controle_material"):
+            raise ValueError("Tabela 'controle_material' não existe")
+    except:
+        raise ValueError("Tabela 'controle_material' não existe")
 
-
-
-    # Insert data into the table
-    # Replace 'controle_material' with your actual table name
-    df.to_sql("controle_material", engine, index=False, if_exists="replace")
+    try:
+        print("Inserindo dados no banco")
+        df.to_sql("controle_material", engine, index=False, if_exists="replace")
+    except Exception as e:
+        print(f"Erro ao inserir dados: {e}")
+    finally:
+        print("Ok")
 
     # Commit the changes and close the connections
-    print("Data inserted successfully.")
     conn.commit()
-    cur.close()
+    cursor.close()
     conn.close()
+    print("Fim do script")
 
 
 if __name__ == "__main__":
     main()
 
 
-## TODO: Arrumar isso pra funcionar se precisar, somente para testes
+# TODO: Arrumar isso pra funcionar se precisar, somente para testes
 # def fetch_all_tables():
-#     cur.execute(
+#     cursor.execute(
 #         """
 #     SELECT table_name
 #     FROM information_schema.tables
 #     WHERE table_schema = 'public' AND table_type = 'BASE TABLE';
 # """
 #     )
-#     tables = cur.fetchall()
+#     tables = cursor.fetchall()
 #     # Print the table names
 #     for table in tables:
 #         print(table[0])
